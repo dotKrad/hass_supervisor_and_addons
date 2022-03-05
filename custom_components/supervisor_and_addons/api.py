80bfc74147009ce68e95e7408929c9935e8ed3fc
Yordan Suarez <ysantiago1985@gmail.com>
2022-03-05 16:01:46 -0500

    remove loop from async call

--- custom_components/supervisor_and_addons/api.py
+++ custom_components/supervisor_and_addons/api.py
@@ -43,7 +43,7 @@ class IntegrationBlueprintApiClient:
     ) -> dict:
         """Get information from the API."""
         try:
-            async with async_timeout.timeout(TIMEOUT, loop=asyncio.get_event_loop()):
+            async with async_timeout.timeout(TIMEOUT):
                 if method == "get":
                     response = await self._session.get(url, headers=headers)
 
